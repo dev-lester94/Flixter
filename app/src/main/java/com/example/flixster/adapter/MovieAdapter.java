4f924f9e63eecb96416d33d8280d35db5dbdcfc6
dev-lester94 <dev.lester94@gmail.com>
2021-02-01 16:28:33 -0800

    addedplaceholderpart1stretchstorespart2

--- app/src/main/java/com/example/flixster/adapter/MovieAdapter.java
+++ app/src/main/java/com/example/flixster/adapter/MovieAdapter.java
@@ -1,22 +1,36 @@
 package com.example.flixster.adapter;
 
+import android.app.Activity;
 import android.content.Context;
+import android.content.Intent;
 import android.content.res.Configuration;
+import android.provider.ContactsContract;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.FrameLayout;
 import android.widget.ImageView;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
+import androidx.core.app.ActivityOptionsCompat;
+import androidx.core.util.Pair;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.bumptech.glide.Glide;
+import com.example.flixster.DetailActivity;
+import com.example.flixster.MainActivity;
 import com.example.flixster.R;
 import com.example.flixster.models.Movie;
 
+import org.parceler.Parcels;
+
 import java.util.List;
+
+import jp.wasabeef.glide.transformations.RoundedCornersTransformation;
 
 public class MovieAdapter extends RecyclerView.Adapter<MovieAdapter.ViewHolder> {
 
@@ -33,6 +47,8 @@ public class MovieAdapter extends RecyclerView.Adapter<MovieAdapter.ViewHolder> 
     @Override
     public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
         Log.d("MovieAdapter", "onCreateViewHolder");
+
+        //Log.d("viewType", ""+viewType);
         View movieView = LayoutInflater.from(context).inflate(R.layout.item_movie, parent, false);
 
         return new ViewHolder(movieView);
@@ -60,16 +76,32 @@ public class MovieAdapter extends RecyclerView.Adapter<MovieAdapter.ViewHolder> 
         TextView tvTitle;
         TextView tvOverview;
         ImageView ivPoster;
+        RelativeLayout container;
+        ImageView ivPlayIcon;
+        ImageView popularMovie;
+        FrameLayout imageLayout;
 
         public ViewHolder(@NonNull View itemView) {
             super(itemView);
             tvTitle = itemView.findViewById(R.id.tvTitle);
             tvOverview = itemView.findViewById(R.id.tvOverview);
             ivPoster = itemView.findViewById(R.id.ivPoster);
+            container = itemView.findViewById(R.id.container);
+
+            ivPlayIcon = itemView.findViewById(R.id.ivPlayIcon);
+
+            popularMovie = itemView.findViewById(R.id.popularMovie);
+            imageLayout = itemView.findViewById(R.id.imageLayout);
 
         }
 
         public void bind(Movie movie) {
+            if(movie.getRating() < 6.5){
+                ivPlayIcon.setVisibility(View.INVISIBLE);
+            }else{
+                //ivPlayIcon = itemView.findViewById(R.id.ivPlayIcon);
+                ivPlayIcon.setVisibility(View.VISIBLE);
+            }
             tvTitle.setText(movie.getTitle());
             tvOverview.setText(movie.getOverview());
             String imageUrl;
@@ -77,13 +109,39 @@ public class MovieAdapter extends RecyclerView.Adapter<MovieAdapter.ViewHolder> 
 
             if(context.getResources().getConfiguration().orientation == Configuration.ORIENTATION_LANDSCAPE){
                 imageUrl = movie.getBackdropPath();
+                Log.d("landscape", "landscape");
+
             }else{
                 imageUrl = movie.getPosterPath();
+                Log.d("portrait", "portrait");
             }
 
+            Glide.with(context)
+                    .load(imageUrl)
+                    .placeholder(R.drawable.ic_launcher_background)
+                    .dontAnimate()
+                    .transform(new RoundedCornersTransformation(15, 5))
+                    .into(ivPoster);
 
 
-            Glide.with(context).load(imageUrl).into(ivPoster);
+
+            //1.Register click listener on the whole container
+
+
+            container.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    //2.Navigate to a new activity on tap
+                    Intent i = new Intent(context, DetailActivity.class);
+                    i.putExtra("movie", Parcels.wrap(movie));
+                    Pair<View, String> p1 = Pair.create((View)tvTitle, "title");
+                    Pair<View, String> p2 = Pair.create((View)tvOverview, "overview");
+                    ActivityOptionsCompat options = ActivityOptionsCompat
+                            .makeSceneTransitionAnimation((Activity) context, p1,p2);
+                    context.startActivity(i, options.toBundle());
+
+                }
+            });
         }
     }
 }
